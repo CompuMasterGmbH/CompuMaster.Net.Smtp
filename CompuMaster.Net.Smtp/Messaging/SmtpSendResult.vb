2af8baaf169ebf72ca9f5f81a06c521b317ca9b5
Jochen Wezel <jwezel@compumaster.de>
2020-01-28 15:12:42 +0100

    SmtpSendResult constructor available for public

--- CompuMaster.Net.Smtp/Messaging/SmtpSendResult.vb
+++ CompuMaster.Net.Smtp/Messaging/SmtpSendResult.vb
@@ -2,10 +2,10 @@
 Option Strict On
 
 Public Class SmtpSendResult
-    Friend Sub New()
+    Public Sub New()
         Me.Success = True
     End Sub
-    Friend Sub New(ex As Exception)
+    Public Sub New(ex As Exception)
         If ex Is Nothing Then Throw New ArgumentNullException(NameOf(ex))
         Me.Success = False
         Me.Exception = ex
